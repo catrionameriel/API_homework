bb594ec9e210efffd9ef5b305bcf795633b855a1
catrionameriel <catriona.meriel@gmail.com>
2018-02-03 12:50:08 +0000

    got rudimentary search working

--- public/app.js
+++ public/app.js
@@ -7,13 +7,41 @@ var makeRequest = function(url, callbackFunction) {
 
 var requestComplete = function() {
   if (this.status !== 200) return;
-  console.log("Hello");
+  var string = JSON.parse(this.responseText)
+  var newsSearch = string.articles;
+  console.log(newsSearch);
+  var container = document.querySelector('#articles-container');
+  showStories(newsSearch, container);
+  showImages(newsSearch);
+}
+
+var showStories = function(search, container) {
+  var number = 0;
+  search.forEach(function(story) {
+    var article = document.createElement('article');
+    article.innerHTML = story.title;
+    article.id = number;
+    container.appendChild(article);
+    number ++;
+  })
+}
+
+var showImages = function(search){
+  var number = 0;
+  search.forEach(function(story) {
+    var article = document.getElementById(number);
+    console.log(article);
+    img = document.createElement('img');
+    img.src = story.urlToImage;
+    article.appendChild(img);
+    number ++;
+  })
 }
 
 var app = function() {
   var key = getApiKey()
   var url = 'https://newsapi.org/v2/everything?' +
-            'q=Apple&' +
+            'q=Oscars&' +
             'from=2018-02-03&' +
             'sortBy=popularity&' +
             'apiKey=' + key;
